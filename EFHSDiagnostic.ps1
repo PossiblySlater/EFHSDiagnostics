01c563e0e9100db991daf33f575f64768237858d
TheCoda <87398108+PossiblySlater@users.noreply.github.com>
2025-05-10 18:54:59 -0700

    Update EFHSDiagnostic.ps1

--- EFHSDiagnostic.ps1
+++ EFHSDiagnostic.ps1
@@ -9,7 +9,7 @@ Start-Transcript -Path $logPath -Force
 
 function Test-ForUpdates {
     $repoUrl = "https://github.com/PossiblySlater/EFHSDiagnostics"
-    $localVersion = "v3.3.1"  # Current version of the script
+    $localVersion = "v3.3.2"  # Current version of the script
 
     Write-Host "Checking for updates..." -ForegroundColor Cyan
     Write-Host ""
@@ -80,7 +80,7 @@ Start-Sleep -Seconds 2
 Clear-Host
 Write-Host "=======================" -ForegroundColor DarkBlue
 Write-Host "    EFHSDIAGNOSTICS" -ForegroundColor Cyan
-Write-Host "     Version 3.3.1" -ForegroundColor Yellow
+Write-Host "     Version 3.3.2" -ForegroundColor Yellow
 Write-Host "    Slater Feistner" -ForegroundColor Red
 Write-Host "=======================`n" -ForegroundColor DarkBlue
 Write-Host ""
